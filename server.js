981c5ca2db45b7529521f882eed5473a5cb3e8ef
IanV27 <ianvillarroel@gmail.com>
2018-12-19 13:42:17 -0500

    changed port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var PORT = process.env.PORT || 5000;
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
+var PORT = process.env.PORT || 5000;
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
@@ -20,5 +20,5 @@ app.set("view engine","handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use("/",routes);
 
-var port = 3000;
-app.listen(port);
+// var port = 3000;
+app.listen(PORT);
